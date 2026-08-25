3eb70bbef84931557348233cfded4a06c90aaa66
Guo Yunhe <i@guoyunhe.me>
2026-08-25 22:33:07 +0800

    i18n

--- wxt.config.ts
+++ wxt.config.ts
@@ -2,5 +2,10 @@ import { defineConfig } from 'wxt';
 
 // See https://wxt.dev/api/config.html
 export default defineConfig({
+  manifest: {
+    name: '__MSG_extensionName__',
+    description: '__MSG_extensionDescription__',
+    default_locale: 'en',
+  },
   modules: ['@wxt-dev/module-react'],
 });
